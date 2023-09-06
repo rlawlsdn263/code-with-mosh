493ad69096fdcd64387902b986ae94825eb13626
rlawlsdn263 <rlawlsdn263@naver.com>
2023-09-06 12:59:09 +0900

    [✍학습] 리스트 렌더링
    - map을 활용한 리스트 렌더링
    _ key prop 에러 해결

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,17 @@
 function ListGroup() {
+  const items = ["뉴욕", "샌프란시사코", "도쿄", "런던", "파리"];
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => {
+          return (
+            <li key={item} className="list-group-item">
+              {item}
+            </li>
+          );
+        })}
       </ul>
     </>
   );
