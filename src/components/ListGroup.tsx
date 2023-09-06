e14adfd9effc06b44f7f4955d6a18b4892338bad
rlawlsdn263 <rlawlsdn263@naver.com>
2023-09-06 14:44:34 +0900

    [✍학습] 상태관리

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,29 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   const items = ["뉴욕", "샌프란시사코", "도쿄", "런던", "파리"];
 
-  //이벤트 핸들러
-  const handleClick = (e: MouseEvent) => console.log(e);
+  //리액트훅
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>리스트 목록 비었음</p>}
       <ul className="list-group">
-        {items.map((item) => {
+        {items.map((item, index) => {
           return (
-            <li key={item} className="list-group-item" onClick={handleClick}>
+            <li
+              key={item}
+              className={
+                selectedIndex === index
+                  ? "list-group-item active"
+                  : "list-group-item"
+              }
+              onClick={() => {
+                setSelectedIndex(index);
+              }}
+            >
               {item}
             </li>
           );
